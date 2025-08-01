54316111d5edcf23409037461c7e25c36239e1f7
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2025-08-01 15:07:09 +0530

    Hero Section Done

---
+++ src/components/HeroOrbit.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { PropsWithChildren } from 'react';
+import StarIcon from "@/assets/icons/star.svg";
+ export  const HeroOrbit = ({children ,size ,rotation}: PropsWithChildren <{ size:number ,rotation :number}>) =>  {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+       <div className=" flex items-start justify-start  " style={{
+        transform: `rotate(${rotation}deg)`,
+        height :`${size}px`,
+        width :`${size}px`,
+       }}>
+       <div className="  inline-flex  " style={{
+         transform: `rotate(${rotation * -1}deg)`,
+       }}>
+          {children}
+       </div>
+       </div>
+        </div>
+  )
+}
+
+ 
+//  animate-spin [animation-duration:30s]"
+//  animate-spin [animation-duration:5s]
