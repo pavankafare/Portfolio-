878e10160f52bbd761a71138363f1662e0c3b1c4
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2025-08-01 16:50:23 +0530

    Featured Project section Done

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,10 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import  CheckCricleIcon  from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+import Image from "next/image";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
@@ -42,5 +46,48 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return  <section className="pb-16 lg:py-24">
+    <div className="container">
+      <div className=" flex justify-center">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text  text-transparent ">Real-world Results</p>
+      </div>
+      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
+      <p className="text-center text-white/60 mt-4 md:text-lg  max-w-md mx-auto lg:text-xl">See how I transformed concepts into engaging digital experiences.</p>
+      <div className="flex flex-col mt-10 md:mt-20 gap-20">
+        {portfolioProjects.map((project ,index)=>(
+          <div className="bg-gray-800 rounded-3xl relative z-0 after:z-10  overflow-hidden after:content-[''] after:absolute after:inset-0  after:outline after:-outline-offset-2 after:rounded-3xl  after:outline-white/20 px-8 pt-8  md:pt-12 md:px-10  lg:pt-16 lg:px-20 after:pointer-events-none"key={index}>
+            <div className="absolute inset-0 -z-10  opacity-5  "style={{
+              backgroundImage:`url(${grainImage.src})` }}></div>
+            <div className="lg:grid  lg:grid-cols-2 lg:gap-16">
+              <div className=" lg:pb-16">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-wider text-sm text-transparent bg-clip-text">
+                  <span>{project.company}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
+
+                <h3 className="font-serif text-2xl  md:text-4xl mt-2 md:mt-5">{project.title}</h3>
+                <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                  {project.results.map((result, resultIndex) => (
+                    <li key={resultIndex} className="flex gap-2 text-sm md:text-base text-white/50">
+                      <CheckCricleIcon className="inline-block w-4 h-4 text-emerald-300 mr-2 md:size-6" key={resultIndex} />
+                      {result.title}</li>
+                  ))}
+                </ul>
+                <a href={project.link}>
+                  <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold md:w-auto inline-flex px-6 items-center justify-center gap-2 mt-8"><span>Visit Live Site</span>
+                    <ArrowUpRightIcon className="size-4" /></button>
+                </a>
+              </div>
+              <div className="relative">
+                <Image className="mt-8 -mb-4 mb:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" src={project.image} alt={project.title} />
+              </div>
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>;
 };
