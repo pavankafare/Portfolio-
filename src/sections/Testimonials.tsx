f01267428a604a30e3a158613fa3428256fa8ac8
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2025-08-03 01:00:01 +0530

    Testimonials section animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -44,10 +44,12 @@ export const TestimonialsSection = () => {
   return <div className="py-16 lg:py-24">
     <div className="container">
       <SectionHeader eyebrow="Happy Clients" title="What Clients Say about Me" description="Don't just take my word for it. See the Work." />
-      <div className="mt-16lg:mt-20 flex overflow-x-clip  [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-        <div className="flex  gap-8 flex-none">
-          {testimonials.map((testimonials) => (
-            <Card className=" max-w-xs md:max-w-md md:p-6" key={testimonials.name}>
+      <div className="mt-12 lg:mt-20 flex overflow-x-clip  [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
+        <div className="flex  gap-8 flex-none animate-move-left [animation-duration:90s] hover:[animation-play-state:paused]">
+          {[...new Array(2)].fill(0).map((_, index) => (
+            <> 
+            {testimonials.map((testimonials) => (
+            <Card className=" max-w-xs md:max-w-md md:p-6 hover:-rotate-3 transition duration-300" key={testimonials.name}>
               <div className="flex gap-4 items-center">
                 <div className="size-14 rounded-full flex-shrink-0 bg-gray-700 inline-flex items-center justify-center">
                   <Image src={testimonials.avatar} alt="h" className="max-h-full" />
@@ -62,6 +64,9 @@ export const TestimonialsSection = () => {
             </Card>
 
           ))}
+          </>
+          ))}
+          
         </div>
       </div>
     </div>
